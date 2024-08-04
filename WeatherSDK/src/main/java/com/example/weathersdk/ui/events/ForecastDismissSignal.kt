cefc0ee40916f5f0291e6dd3192aa0493fbadd69
Daniyal Idrees <>
2024-08-04 21:13:23 +0200

    kdoc added for forecastDismissSignal

--- WeatherSDK/src/main/java/com/example/weathersdk/ui/events/ForecastDismissSignal.kt
+++ WeatherSDK/src/main/java/com/example/weathersdk/ui/events/ForecastDismissSignal.kt
@@ -5,13 +5,32 @@ import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
-
+/**
+ * Responsible for broadcasting [FinishEvent] at the time of dismissal of the forecast screen.
+ *
+ * This class uses a [MutableSharedFlow] to emit events that signify different dismissal scenarios. The events
+ * can be observed by components interested in handling these dismissal signals.
+ */
 @Singleton
 class ForecastDismissSignal @Inject constructor() {
     private val _events = MutableSharedFlow<FinishEvent>()
+
+    /**
+     * A [SharedFlow] that emits [FinishEvent] related to the dismissal of the forecast screen.
+     *
+     * Observers can collect from this flow to receive notifications about different [FinishEvent]s.
+     */
     val events: SharedFlow<FinishEvent>
         get() = _events
 
+    /**
+     * Internal use only.
+     *
+     * Emits a [FinishEvent] to the [events] flow. This method is used to signal different dismissal
+     * events.
+     *
+     * @param event The [FinishEvent] to be emitted.
+     */
     internal suspend fun emitEvent(event: FinishEvent) {
         _events.emit(event)
     }
