a09823c6b79c0f70c10f77f4623a1ac0ae191d92
Daniyal Idrees <>
2024-08-05 13:55:24 +0200

    import fix due to sdkkeymanager file movement

--- WeatherSDK/src/main/java/com/example/weathersdk/di/SdkKeyEntryPoint.kt
+++ WeatherSDK/src/main/java/com/example/weathersdk/di/SdkKeyEntryPoint.kt
@@ -1,6 +1,6 @@
 package com.example.weathersdk.di
 
-import com.example.weathersdk.data.SdkKeyManager
+import com.example.weathersdk.SdkKeyManager
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
