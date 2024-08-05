918bec474e546bce3ba9cbea4ac894e738f41850
Daniyal Idrees <>
2024-08-05 16:18:05 +0200

    kdoc comment

--- WeatherSDK/src/main/java/com/example/weathersdk/WeatherSdkInstanceProvider.kt
+++ WeatherSDK/src/main/java/com/example/weathersdk/WeatherSdkInstanceProvider.kt
@@ -3,6 +3,9 @@ package com.example.weathersdk
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Internal use only.
+ */
 @Singleton
 internal class WeatherSdkInstanceProvider @Inject constructor() {
     fun get() = WeatherSdk.getInstance()
